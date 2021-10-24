9099d1d1bca9506c02fcf1e776063c0fb86cbfde
Kwasi <cubesprince@gmail.com>
2021-10-24 01:47:32 +0100

    fixed error and seeded 20 products and category

--- products/models.py
+++ products/models.py
@@ -6,19 +6,23 @@ class Product(models.Model):
     brand = models.CharField(max_length = 50, default = None)
     name = models.CharField(max_length = 100, default = None)
     description = models.CharField(max_length = 500, default = None)
-    price = models.CharField(max_length = 50,default=None)
+    price = models.IntegerField(default=None)
     size = ArrayField(models.CharField(max_length = 50), blank=True)
     highlight = ArrayField(models.CharField(max_length = 50), blank=True)
     images = ArrayField(models.CharField(max_length = 300), blank=True)
+    composition = models.CharField(max_length = 50, default = None)
+    washing_instruction = models.CharField(max_length = 50, default = None)
+    wearing = models.CharField(max_length = 50, default = None)
     category = models.ManyToManyField(
         'category.Category',
         related_name= "products"
     )
-    in_stock = models.BooleanField(default = True)
     owner = models.ManyToManyField(
         'jwt_auth.User',
         related_name="products"
     )
+    in_stock = models.BooleanField(default = True)
+    
 
     def __str__(self):
         return f"{self.brand} - {self.name} - {self.price}"
